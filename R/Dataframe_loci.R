a571537d0fcf12446f81f6b0965e186425da0f3f
jakob949 <s213428@dtu.dk>
2022-04-26 13:27:48 +0000

    v_26-04

--- R/Dataframe_loci.R
+++ R/Dataframe_loci.R
@@ -23,7 +23,14 @@ ff <- f %>% select(-pop)
 ff1 <- ff %>% mutate_all (A1)
 ff2 <- ff %>% mutate_all (A2)
 
-ff1 <- rename_with(ff1, name1)
-ff2 <- rename_with(ff2, name2)
+#ff1 <- rename_with(ff1, name1)
+#ff2 <- rename_with(ff2, name2)
 
-
+for (i in 1:(length(header)-1)) {
+  v1 <- ff1 %>% pull(i)
+  v2 <- ff2 %>% pull(i)
+  v3 <- as_tibble(c(v1, v2))
+  tt=(v3 %>% group_by(value) %>% tally())
+  print (tt)
+}
+print(sum(tt[2]))
