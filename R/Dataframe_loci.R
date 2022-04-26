792fe49efc887f728898502f578d9e51daffc963
jakob949 <s213428@dtu.dk>
2022-04-26 16:22:52 +0000

    Calculation of freqs

--- R/Dataframe_loci.R
+++ R/Dataframe_loci.R
@@ -26,11 +26,46 @@ ff2 <- ff %>% mutate_all (A2)
 #ff1 <- rename_with(ff1, name1)
 #ff2 <- rename_with(ff2, name2)
 
-for (i in 1:(length(header)-1)) {
+v1 <- ff1 %>% pull(1)
+v2 <- ff2 %>% pull(1)
+v3 <- as_tibble(c(v1, v2))
+tt = (v3 %>% group_by(value) %>% tally())
+total_allele <- (sum(rowSums(tt[,c(2)])))
+tidy <- as_tibble(tt)
+tidy <- tidy %>% mutate (Indiv =loci_id[2] )
+
+
+
+
+for (i in 2:(length(loci_id)-1)) {
   v1 <- ff1 %>% pull(i)
   v2 <- ff2 %>% pull(i)
   v3 <- as_tibble(c(v1, v2))
-  tt=(v3 %>% group_by(value) %>% tally())
-  print (tt)
+  tt = (v3 %>% group_by(value) %>% tally())
+  total_allele <- (sum(rowSums(tt[,c(2)])))
+  #print(loci_id[i+1])
+  #print(tt)
+
+
+  tidy<-tidy %>% t %>% as.data.frame
+  tt <- tt %>% add_column (Indiv =loci_id[i+1] )
+  tt<-tt %>% t %>% as.data.frame
+  tidy<-tidy %>% add_column(tt)
+  
+  tidy<-tidy %>% t %>% as.data.frame
 }
-print(sum(tt[2]))
+
+tidy <- tidy %>% mutate (total_Al = total_allele)
+
+tidy <- as_tibble(tidy)
+
+n1 <- tidy %>% 
+  select(n) %>%
+  pull(1) %>%
+  as.integer()
+
+tidy <- tidy %>% 
+  mutate (n =n1 )
+
+tidy <- tidy %>% 
+  mutate (freq = n/total_Al)
