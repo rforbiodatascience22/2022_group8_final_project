b76ea80c825140b86eb37605b0f740ef0c7b97fd
jakob949 <s213428@dtu.dk>
2022-04-26 10:45:26 +0000

    New file, changes...

--- R/tidy1.R
+++ R/tidy1.R
@@ -1,6 +1,8 @@
+library('tidyverse')
 data <- read_lines("/cloud/project/data/Lithobates pipiens/Lithobates pipiens_LP1-A.txt", skip = 1) 
 ncols = match('pop', data)-1
 loci_id=rev(data[1:(ncols+1)])
+
 
 f <- read.delim("/cloud/project/data/Lithobates pipiens/Lithobates pipiens_LP1-A.txt", header=TRUE)
 for (i in 1:nrow(f)){
@@ -10,3 +12,6 @@ for (i in 1:nrow(f)){
 f <- f %>% 
   slice(n = ncols+2:nrow(f)) %>% 
   separate(colnames(f[1]),into= loci_id, sep=" ")
+
+
+header <= loci_id
