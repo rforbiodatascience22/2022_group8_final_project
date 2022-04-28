d43e3118941643aa1dba65180c03774c564448ff
serestech <s212716@dt.dk>
2022-04-28 16:17:51 +0000

    sample_ij but with errors

--- R/linkage_deseq.R
+++ R/linkage_deseq.R
@@ -1,39 +1,36 @@
 divide_alleles <-function(string){
-  if (str_length(string[[1]])==4){
-    alleles <- (str_match_all(string, "\\d{2}"))
-  }else if (str_length(string[[1]])==6){
-    alleles <- (str_match_all(string, "\\d{3}"))
-  }
+  alleles <- case_when(str_length(string[[1]])==4 ~ str_match_all(string, "\\d{2}"),
+                       str_length(string[[1]])==6 ~ str_match_all(string, "\\d{3}"))
   return(alleles)
 }
 
 freq_function <- function(unique, total){
   freq <- 0
-  for (i in total[[1]]){
+  for (i in total){
     if (unique==i){
       freq <- freq + 1
     }
   }
-  freq <- freq/length(total[[1]])
+  freq <- freq/length(total)
   return(freq)
 }
 
 homo_freq_function <- function(unique, pairs){
   freq <- 0
-  for (i in pairs[[1]]){
+  for (i in pairs){
     if (i[[1]]==unique & i[[2]]==unique){
       freq <- freq + 1
     }
   }
-  freq <- freq/length(pairs[[1]])
+  freq <- freq/length(pairs)
   return(freq)
 }
 
 x_vector_function <- function(unique, pairs){
   x <- c()
-  for (i in pairs[[1]]){
+  for (i in pairs){
     if (i[[1]]=='000'){
-      x <- append(x,'none')
+      x <- append(x,NA_integer_)
     } else if (i[[1]]==unique & i[[2]]==unique){
       x <- append(x,-1)
     } else if ((i[[1]]==unique & i[[2]]!=unique) | (i[[1]]!=unique & i[[2]]==unique)){
@@ -80,7 +77,7 @@ df_loci_tidy <- df_loci %>%
   unnest(unique_alleles) %>%
   group_by(locus) %>%
   mutate(k=n()) %>%                                        #add number of unique alleles per locus
-  group_by(unique_alleles,locus) %>%
+  rowwise %>%
   mutate(freq=freq_function(unique_alleles,total_alleles), #add total allele frequencies 
          h=homo_freq_function(unique_alleles,allele_pairs), #add homozygote frequencies
          x=list(x_vector_function(unique_alleles,allele_pairs))) %>% #add x vector with allele info
--- R/linkage_deseq2.R
+++ R/linkage_deseq2.R
@@ -1,6 +1,25 @@
+sample_ij_function <- function(i,j){
+  x_i <- df_loci_tidy %>%
+    filter(locus==i) %>%
+    pull(x)
+  x_j <- df_loci_tidy %>%
+    filter(locus==j) %>%
+    pull(x)
+  sample_ij <- tibble(x_i=x_i[[1]],x_j=x_j[[1]]) %>%
+    mutate(valid=case_when(x_i==NA_integer_ | x_j==NA_integer_ ~ 0,
+                         TRUE ~ 1)) %>%
+    summarise(n=n())
+  return(sample_ij)
+}
+
+s <- sample_ij_function("Rpi106","Rpi107")
+
+
 df_combi <- df_loci %>%
   select(locus) %>%
   rename(i=locus) %>%
   mutate(j=list(i)) %>%
   unnest(j) %>%
-  filter(i!=j)
+  filter(i!=j) %>%
+  rowwise %>%
+  mutate(S_ij=sample_ij_function(i,j))
