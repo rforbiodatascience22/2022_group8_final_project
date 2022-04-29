22f9c30d3dd90bbaebfe894d311f50a70a3d6249
serestech <s212716@dt.dk>
2022-04-29 13:09:14 +0000

    added delta rAB and rij calculations, but it takes 30 mins for the file we are testing

--- R/linkage_deseq2.R
+++ R/linkage_deseq2.R
@@ -26,8 +26,58 @@ allele_combi_function <- function(i,j){
   return(combis)
 }
 
-delta_AB_function <- function(i,j,A,B){
+delta_AB_function <- function(i,j,A,B,S_ij){
   
+  #Extract x vectors from df_loci_tidy
+  x_A <- df_loci_tidy %>%
+    ungroup %>%
+    filter(locus==i & unique_alleles==A) %>%
+    select(x) %>%
+    unnest(x) %>%
+    pull(x)
+  x_B <- df_loci_tidy %>%
+    ungroup %>%
+    filter(locus==j & unique_alleles==B) %>%
+    select(x) %>%
+    unnest(x) %>%
+    pull(x)
+
+  #Remove samples with missing data in any of them
+  x_A_B <- tibble(x_A=x_A,x_B=x_B) %>%
+    filter(!(is.na(x_A) | is.na(x_B)))
+  x_A <- x_A_B %>%
+    pull(x_A)
+  x_B <- x_A_B %>%
+    pull(x_B)
+  
+  #Calculate delta following the formula from the algorithm
+  delta_AB <- (S_ij/(S_ij-1))*(S_ij*sum(x_A*x_B)-sum(x_A)*sum(x_B))/(2*S_ij**2)
+  return(delta_AB)
+}
+
+r_AB_function <- function(i,j,A,B,delta_AB){
+  
+  #Retrieve data (freq,h)
+  freq_A <- df_loci_tidy %>%
+    ungroup %>%
+    filter(locus==i & unique_alleles==A) %>%
+    pull(freq)
+  h_A <- df_loci_tidy %>%
+    ungroup %>%
+    filter(locus==i & unique_alleles==A) %>%
+    pull(h)
+  freq_B <- df_loci_tidy %>%
+    ungroup %>%
+    filter(locus==j & unique_alleles==B) %>%
+    pull(freq)
+  h_B <- df_loci_tidy %>%
+    ungroup %>%
+    filter(locus==j & unique_alleles==B) %>%
+    pull(h)
+  
+  #Calculate r_AB following the formula in the algorithm
+  r_AB <- delta_AB/(sqrt((freq_A*(1-freq_A)+(h_A-freq_A**2))*(freq_B*(1-freq_B)+(h_B-freq_B**2))))
+  return(r_AB)
 }
 
 
@@ -41,3 +91,13 @@ df_combi <- df_loci %>%
   mutate(S_ij=sample_ij_function(i,j)) %>%
   mutate(allele_combinations=list(allele_combi_function(i,j))) %>%
   unnest(allele_combinations) %>%
+  rowwise %>%
+  mutate(delta_AB=delta_AB_function(i,j,A,B,S_ij),
+         r_AB=r_AB_function(i,j,A,B,delta_AB))
+
+df_combi_tidy <- df_combi %>%
+  group_by(i,j) %>%
+  mutate(r_ij=mean(r_AB)) %>%
+  nest(allele_combinations=c(A,B,delta_AB,r_AB)) #%>%
+  #select(-allele_combinations)
+  
