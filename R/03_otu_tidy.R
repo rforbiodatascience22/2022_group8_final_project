2e0aa6c136ed36043f9bc20429b6e9c372b1d329
EnriqueMD <s220670@dtu.dk>
2022-05-02 09:40:50 +0000

    tidy data in OTU file

---
+++ R/03_otu_tidy.R
@@ -0,0 +1,7 @@
+data <- read_tsv("data/otu.tsv") %>% 
+  as_tibble() %>% 
+  mutate(`Consensus Lineage` = str_remove_all(`Consensus Lineage`, "[:alpha:][:punct:]{2}")) %>% 
+  separate(col = `Consensus Lineage`, into = c('realm', 'phylum', 'class', 'order', 'family', 'genus', 'species'), sep = ';' ) %>% 
+  select(-c(`#OTU ID`, genus, species, phylum, realm, class, order)) %>% 
+  group_by(family)
+data
