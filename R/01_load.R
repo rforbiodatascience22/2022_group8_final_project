2e03811c10851e5776085aa2cdb369809d8472e5
paulamli <paulapolls@hotmail.com>
2022-05-02 08:21:50 +0000

    organization

--- R/01_load.R
+++ R/01_load.R
@@ -1,7 +1,9 @@
 ## CFU COUNTS
 cfu_df <- read_csv("_raw/cfu_counts.csv") %>%
   as_tibble
-  
+
+write_tsv(cfu_df, file='data/cfu.tsv')
+
 ## MAPFILE  
 map_df_cols <- read_lines("_raw/mapfile.txt",n_max=1) %>%
   str_split("\\t",simplify = TRUE)
@@ -10,7 +12,7 @@ map_df <- read_lines("_raw/mapfile.txt",skip=1,skip_empty_rows = TRUE) %>%
   as_tibble_col(column_name = "lines") %>%
   separate(lines,map_df_cols,sep="\\t")
 
-write_tsv(otu_df, file='data/map.tsv')  
+write_tsv(map_df, file='data/map.tsv')  
  
 ## OTUFILE 
 otu_df_cols <- read_lines("_raw/otufile.txt",n_max=1) %>%
