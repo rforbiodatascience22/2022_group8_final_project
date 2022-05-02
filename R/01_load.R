541c026528c7d20ede792f950c0b033a3351b902
serestech <s212716@dt.dk>
2022-05-02 08:18:17 +0000

    fix

--- R/01_load.R
+++ R/01_load.R
@@ -1,7 +1,7 @@
 cfu_df <- read_csv("_raw/cfu_counts.csv") %>%
   as_tibble
 
-write_tsv(otu_df, file='data/cfu.tsv')  
+write_tsv(cfu_df, file='data/cfu.tsv')  
   
 map_df_cols <- read_lines("_raw/mapfile.txt",n_max=1) %>%
   str_split("\\t",simplify = TRUE)
@@ -10,7 +10,7 @@ map_df <- read_lines("_raw/mapfile.txt",skip=1,skip_empty_rows = TRUE) %>%
   as_tibble_col(column_name = "lines") %>%
   separate(lines,map_df_cols,sep="\\t")
 
-write_tsv(otu_df, file='data/map.tsv')  
+write_tsv(map_df, file='data/map.tsv')  
   
 otu_df_cols <- read_lines("_raw/otufile.txt",n_max=1) %>%
   str_split("\\t",simplify = TRUE)
