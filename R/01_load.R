15456fff38ab51485e9b193260b17dfa40fb3ded
jakob949 <s213428@dtu.dk>
2022-05-02 07:53:42 +0000

    Write tsv

--- R/01_load.R
+++ R/01_load.R
@@ -1,19 +1,21 @@
-cfu_df <- read_csv("data/cfu_counts.csv") %>%
+cfu_df <- read_csv("_raw/cfu_counts.csv") %>%
   as_tibble
   
   
-map_df_cols <- read_lines("data/mapfile.txt",n_max=1) %>%
-  str_split("\\t")
+map_df_cols <- read_lines("_raw/mapfile.txt",n_max=1) %>%
+  str_split("\\t",simplify = TRUE)
 
-map_df <- read_lines("data/mapfile.txt",skip=1,skip_empty_rows = TRUE) %>%
+map_df <- read_lines("_raw/mapfile.txt",skip=1,skip_empty_rows = TRUE) %>%
   as_tibble_col(column_name = "lines") %>%
-  separate(lines,map_df_cols[[1]],sep="\\t")
+  separate(lines,map_df_cols,sep="\\t")
   
   
-  
-otu_df_cols <- read_lines("data/otufile.txt",n_max=1) %>%
-  str_split("\\t")
+otu_df_cols <- read_lines("_raw/otufile.txt",n_max=1) %>%
+  str_split("\\t",simplify = TRUE)
 
-otu_df <- read_lines("data/otufile.txt",skip=1,skip_empty_rows = TRUE) %>%
+otu_df <- read_lines("_raw/otufile.txt",skip=1,skip_empty_rows = TRUE) %>%
   as_tibble_col(column_name = "lines") %>%
-  separate(lines,otu_df_cols[[1]],sep="\\t")
+  separate(lines,otu_df_cols,sep="\\t")
+
+
+write.table(otu_df, file='data/otu.tsv')
---
+++ R/02_clean.R
@@ -0,0 +1,4 @@
+library('tidyverse')
+test <- read_tsv('data/otu.tsv')
+
+test = as_tibble 
