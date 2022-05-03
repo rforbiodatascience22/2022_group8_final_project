ad888ee4a9628618e8316005c275be77b382fe28
jakob949 <s213428@dtu.dk>
2022-05-03 12:59:37 +0000

    Leon Layout

--- R/05_model_ii.R
+++ R/05_model_ii.R
@@ -1,12 +1,21 @@
 data <- cfu_df_clean
-p1 <- ggplot(data, mapping = aes(x = Antibiotic, y = cfu_ml)) +
+p1 <- ggplot(data, 
+             mapping = aes(x = Antibiotic, 
+                           y = cfu_ml)) +
   geom_boxplot() +
   coord_flip()
-p2 <- ggplot(data, mapping = aes(x = Donor, y = cfu_ml)) +
+
+p2 <- ggplot(data, 
+             mapping = aes(x = Donor, 
+                           y = cfu_ml)) +
   geom_boxplot() +
   coord_flip()  
+
 p3 <- ggplot(data) +
-  geom_bar(mapping = aes(x = Antibiotic, y = cfu_ml), stat = "identity") +
+  geom_bar(mapping = aes(x = Antibiotic, 
+                         y = cfu_ml), 
+           stat = "identity") +
   facet_wrap(~Donor)
+
 p3 | (p1 / p2)
   
