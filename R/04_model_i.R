aaadacf5b8099c9c9f565e0a052b6351212a0abe
jakob949 <s213428@dtu.dk>
2022-05-03 12:45:12 +0000

    Violin plot

--- R/04_model_i.R
+++ R/04_model_i.R
@@ -11,3 +11,19 @@ heat_map <- heat_map_data %>%
   facet_grid(cols = vars(Donor), space = "free_x")
 
 heat_map
+
+violin_1_A <- read_tsv("data/cfu_clean.tsv") %>%
+  select(c(Donor, Community, Antibiotic, Replicate, cfu_ml))
+
+violin_1_B <- filter_at(violin_1_A, vars(cfu_ml), all_vars((.) != 0))
+
+
+ggplot(data = violin_1_B,
+       mapping = aes(x = Antibiotic, 
+                     y = cfu_ml,
+                     fill = Community,
+                     group = interaction(Antibiotic, Community))) +
+  geom_violin() +
+  geom_point(position = position_dodge(width=0.9))
+
+  
