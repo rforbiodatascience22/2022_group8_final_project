fc9da389a2914f2e4a31ae1932fcacf935d1e8e0
EnriqueMD <s220670@dtu.dk>
2022-05-03 09:31:49 +0000

    Starting plots from the cfu file

---
+++ R/05_model_ii.R
@@ -0,0 +1,12 @@
+data <- cfu_df_clean
+p1 <- ggplot(data, mapping = aes(x = Antibiotic, y = cfu_ml)) +
+  geom_boxplot() +
+  coord_flip()
+p2 <- ggplot(data, mapping = aes(x = Donor, y = cfu_ml)) +
+  geom_boxplot() +
+  coord_flip()  
+p3 <- ggplot(data) +
+  geom_bar(mapping = aes(x = Antibiotic, y = cfu_ml), stat = "identity") +
+  facet_wrap(~Donor)
+p3 | (p1 / p2)
+  
