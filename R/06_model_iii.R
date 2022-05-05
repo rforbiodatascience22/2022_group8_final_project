d9ab79c77431462bb622546cc2884d298bfd0a13
EnriqueMD <s220670@dtu.dk>
2022-05-05 14:27:45 +0000

    starting the Rmarkdown file

--- R/06_model_iii.R
+++ R/06_model_iii.R
@@ -4,11 +4,12 @@ violin_1_A <- read_tsv("data/cfu_clean.tsv") %>%
 violin_1_B <- filter_at(violin_1_A, vars(cfu_ml), all_vars((.) != 0))
 
 
+
 ggplot(data = violin_1_B,
        mapping = aes(x = Antibiotic, 
                      y = cfu_ml,
                      fill = Community,
                      group = interaction(Antibiotic, Community))) +
   geom_violin() +
-  ylim(1e7,1e9) +
-  geom_point(position = position_dodge(width=0.9))
+  geom_point(position = position_dodge(width=0.9)) + 
+  ylim(100000, 1000000000)
