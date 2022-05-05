102874d02de6091b7a8a31b17bd764fdc59c28f1
bernatgodayol <s212717@dtu.dk>
2022-05-05 17:39:00 +0000

    Success in splitting geom_points :)

--- R/06_model_iii.R
+++ R/06_model_iii.R
@@ -1,14 +1,21 @@
 violin_1_A <- read_tsv("data/cfu_clean.tsv") %>%
-  select(c(Donor, Community, Antibiotic, Replicate, cfu_ml))
+  select(c(Donor, Community, Antibiotic, Replicate, cfu_ml)) %>%
+  mutate(cfu_ml = na_if(cfu_ml, 0) ) %>%
+  drop_na()
 
-violin_1_B <- filter_at(violin_1_A, vars(cfu_ml), all_vars((.) != 0))
+
+ggplot(data = violin_1_A,
+       mapping = aes(x = Antibiotic,
+                     y = cfu_ml)) +
+  geom_violin(mapping = aes(fill = Community)) +
+  geom_point(mapping = aes(shape = factor(Donor),
+                           group = interaction(Donor, Community)),
+             position = position_dodge(width = 0.75)) +
+  scale_y_log10() 
+
+  
 
 
-ggplot(data = violin_1_B,
-       mapping = aes(x = Antibiotic, 
-                     y = cfu_ml,
-                     fill = Community,
-                     group = interaction(Antibiotic, Community))) +
-  geom_violin() +
-  ylim(1e7,1e9) +
-  geom_point(position = position_dodge(width=0.9))
+
+
+
