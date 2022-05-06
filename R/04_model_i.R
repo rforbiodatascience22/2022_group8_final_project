d1c748f3976f473e2a77a363c28b23de8e86f920
serestech <s212716@dt.dk>
2022-05-06 15:44:22 +0000

    heatmap

--- R/04_model_i.R
+++ R/04_model_i.R
@@ -1,17 +1,66 @@
 #families <- c(Ruminococcaceae,Enterobacteriaceae,Lachnospiraceae,Bifidobacteriaceae,Clostridiaceae,Erysipelotrichaceae,Bacteroidaceae,Coriobacteriaceae,Porphyromonadaceae,Enterococcaceae)
 
 heat_map_data <- read_tsv("data/otu_map_merged.tsv", show_col_types = FALSE) %>%
-  pivot_longer(c(Ruminococcaceae,Enterobacteriaceae,Lachnospiraceae,Bifidobacteriaceae,Clostridiaceae_1,Erysipelotrichaceae,Bacteroidaceae,Coriobacteriaceae,Porphyromonadaceae,Enterococcaceae),
+  rename(Clostridiaceae=Clostridiaceae_1) %>%
+  pivot_longer(c(Ruminococcaceae,Enterobacteriaceae,Lachnospiraceae,Bifidobacteriaceae,Clostridiaceae,Erysipelotrichaceae,Bacteroidaceae,Coriobacteriaceae,Porphyromonadaceae,Enterococcaceae),
                names_to = "family",
-               values_to = "counts")
+               values_to = "counts") %>%
+  mutate(Antibiotic = case_when(Time == "0h" ~ "0h",
+                                Antibiotic == "none" ~ "AB free",
+                                Antibiotic == "Polymyxin" ~ "Poly",
+                                Antibiotic == "Rifampicin" ~ "Rif"),
+         Replicate = str_extract(Name,"\\d"))
 
-heat_map <- heat_map_data %>%
-  ggplot(mapping = aes(Name, family)) +
-  geom_tile(aes(fill = counts)) +
-  facet_grid(cols = vars(Donor), space = "free_x")
+heat_map_data_1 <- heat_map_data %>%
+  filter(Donor==1)
+
+heat_map_data_2 <- heat_map_data %>%
+  filter(Donor==2)
+
+heat_map_data_3 <- heat_map_data %>%
+  filter(Donor==3)
+
+h1 <- heat_map_data_1 %>%
+  ggplot(mapping = aes(Replicate, family)) +
+  geom_raster(aes(fill = counts)) +
+  facet_grid(.~Antibiotic, scales = "free", space = "free", switch = "x") +
+  theme_minimal() +
+  labs(title = "Donor 1",
+       x = NULL,
+       y = NULL) +
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x=element_blank(),
+        legend.position = "none",
+        panel.spacing = unit(0, "lines"))
+
+h2 <- heat_map_data_2 %>%
+  ggplot(mapping = aes(Replicate, family)) +
+  geom_raster(aes(fill = counts)) +
+  facet_grid(.~Antibiotic, scales = "free", space = "free", switch = "x") +
+  theme_minimal() +
+  labs(title = "Donor 2",
+       x = "Treatment",
+       y = NULL) +
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        legend.position = "none",
+        panel.spacing = unit(0, "lines"),
+        plot.margin = margin(t=0, r=0, b=0, l=0))
+
+h3 <- heat_map_data_3 %>%
+  ggplot(mapping = aes(Replicate, family)) +
+  geom_raster(aes(fill = counts)) +
+  facet_grid(.~Antibiotic, scales = "free", space = "free", switch = "x") +
+  theme_minimal() +
+  labs(title = "Donor 3",
+       x = NULL,
+       y = NULL) +
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        panel.spacing = unit(0, "lines"))
+
+heat_map <- h1 | h2 | h3
 
 heat_map
-
-
-
-  
