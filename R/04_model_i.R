cb7dfaf798795338d140f5183d0c53b8688b5bb9
serestech <s212716@dt.dk>
2022-05-06 18:07:53 +0000

    heatmap requetederechupete

--- R/04_model_i.R
+++ R/04_model_i.R
@@ -12,7 +12,9 @@ heat_map_data <- read_tsv("data/otu_map_merged.tsv", show_col_types = FALSE) %>%
                                 Antibiotic == "none" ~ "AB free",
                                 Antibiotic == "Polymyxin" ~ "Poly",
                                 Antibiotic == "Rifampicin" ~ "Rif"),
-         Replicate = str_extract(Name,"\\d"))
+         Replicate = str_extract(Name, "\\d")) %>%
+  group_by(SampleID) %>%
+  mutate(counts_perc = 100*counts/sum(counts))
 
 heat_map_data_1 <- heat_map_data %>%
   filter(Donor==1)
@@ -25,44 +27,67 @@ heat_map_data_3 <- heat_map_data %>%
 
 h1 <- heat_map_data_1 %>%
   ggplot(mapping = aes(Replicate, family)) +
-  geom_raster(aes(fill = counts)) +
+  geom_tile(aes(fill = counts_perc), colour="white") +
   facet_grid(.~Antibiotic, scales = "free", space = "free", switch = "x") +
   theme_minimal() +
   labs(title = "Donor 1",
        x = NULL,
        y = NULL) +
-  theme(plot.title = element_text(hjust = 0.5),
+  scale_fill_gradient2(name = "Abundance (%)",
+                       low = "#336699",
+                       high = "#FF3300", 
+                       mid = "orange",
+                       midpoint = 2.9,
+                       trans = "pseudo_log",
+                       limits = c(0,75)) +
+  theme(plot.title = element_text(size = 10, hjust = 0.5),
         axis.text.x=element_blank(),
         legend.position = "none",
-        panel.spacing = unit(0, "lines"))
+        panel.spacing = unit(0, "lines"),
+        panel.grid.major = element_blank())
 
 h2 <- heat_map_data_2 %>%
   ggplot(mapping = aes(Replicate, family)) +
-  geom_raster(aes(fill = counts)) +
+  geom_tile(aes(fill = counts_perc), colour="white") +
   facet_grid(.~Antibiotic, scales = "free", space = "free", switch = "x") +
   theme_minimal() +
   labs(title = "Donor 2",
        x = "Treatment",
        y = NULL) +
-  theme(plot.title = element_text(hjust = 0.5),
+  scale_fill_gradient2(name = "Abundance (%)",
+                       low = "#336699",
+                       high = "#FF3300",
+                       mid = "orange", midpoint = 2.9,
+                       trans = "pseudo_log",
+                       limits = c(0,75)) +
+  theme(plot.title = element_text(size = 10, hjust = 0.5),
         axis.text.x=element_blank(),
         axis.text.y=element_blank(),
         legend.position = "none",
         panel.spacing = unit(0, "lines"),
-        plot.margin = margin(t=0, r=0, b=0, l=0))
+        plot.margin = margin(r=0, l=0),
+        panel.grid.major = element_blank())
 
 h3 <- heat_map_data_3 %>%
   ggplot(mapping = aes(Replicate, family)) +
-  geom_raster(aes(fill = counts)) +
+  geom_tile(aes(fill = counts_perc), colour="white") +
   facet_grid(.~Antibiotic, scales = "free", space = "free", switch = "x") +
   theme_minimal() +
   labs(title = "Donor 3",
        x = NULL,
        y = NULL) +
-  theme(plot.title = element_text(hjust = 0.5),
+  scale_fill_gradient2(name = "Abundance (%)",
+                       low = "#336699",
+                       high = "#FF3300",
+                       mid = "orange",
+                       midpoint = 2.9,
+                       trans = "pseudo_log",
+                       limits = c(0,75)) +
+  theme(plot.title = element_text(size = 10, hjust = 0.5),
         axis.text.x=element_blank(),
         axis.text.y=element_blank(),
-        panel.spacing = unit(0, "lines"))
+        panel.spacing = unit(0, "lines"),
+        panel.grid.major = element_blank())
 
 heat_map <- h1 | h2 | h3
 
