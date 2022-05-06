38fcbe8b8ace623d5f805412e3c21b4711e0ba43
jakob949 <s213428@dtu.dk>
2022-05-06 12:16:07 +0000

    ---

--- R/07_model_iv.R
+++ R/07_model_iv.R
@@ -1,22 +1,39 @@
+library("tidyverse")
+
 merge_Rifampicin <- read_tsv('data/otu_map_merged.tsv') %>%
   filter (Antibiotic == 'Rifampicin') %>%
   select(9:56) %>%
   sum()
 
 merge_Polymyxin <- read_tsv('data/otu_map_merged.tsv') %>%
-  filter (Antibiotic == 'Polymyxin', Donor ==1 ) %>%
-  select(9:56) %>%
-  slice(2) %>%
-  sum()
+  filter (Antibiotic == 'Polymyxin') %>%
+  select(9:56)
+  #slice(3) %>%
+
   
-print(10e-3*(merge_Polymyxin))  
+print((merge_Polymyxin/3))  
  
-cfu <- read_tsv("data/cfu_clean.tsv") %>%
-  filter (Antibiotic == 'P', Donor == 2, Community == "-") %>%
-  select(cfu_ml) %>%
-  mutate(cfu_nl = cfu_ml*1e-6)
+
 
 merge_none <- read_tsv('data/otu_map_merged.tsv') %>%
   filter (Antibiotic == 'none') %>%
   select(9:56) %>%
   sum()
+
+
+cfu <- read_tsv("data/cfu_clean.tsv") %>%
+  #filter (Community == "+") %>%
+  select(c(Donor, Community, Antibiotic, Replicate, cfu_ml)) %>%
+  mutate(cfu_nl = cfu_ml*1e-6)
+
+ggplot(data = cfu,
+       mapping = aes(x = Antibiotic,
+                     y = cfu_nl)) +
+  geom_violin(mapping = aes(fill = Antibiotic)) +
+  geom_point(mapping = aes(shape = factor(Donor),
+                           group = Donor),
+             position = position_dodge(width = 0.3)
+             )
+             
+             
+             
