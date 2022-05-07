4a119065084330dc9bdf00249b321e4508211fcb
jakob949 <s213428@dtu.dk>
2022-05-07 12:07:01 +0000

    pkjåoeqk

--- R/07_model_iv.R
+++ R/07_model_iv.R
@@ -1,7 +1,16 @@
 cfu <- read_tsv("data/cfu_clean.tsv") %>%
   #filter (Community == "+") %>%
-  select(c(Donor, Community, Antibiotic, Replicate, cfu_ml)) %>%
+  select(c(Donor, Antibiotic, cfu_ml)) %>%
   mutate(cfu_nl = cfu_ml*1e-6)
+
+otu <- read_tsv("data/otu_map_merged.tsv") %>%
+    select(c(9:52)) %>%
+    slice (1) %>%
+    sum()
+    #mutate(cfu_nl = cfu_ml*1e-6)
+
+
+
 
 ggplot(data = cfu,
        mapping = aes(x = Antibiotic,
