1282e7169b8c0ecbb1f53b33509c318abd094ef8
paulamli <paulapolls@hotmail.com>
2022-05-07 17:04:02 +0000

    comments

--- R/03_augment.R
+++ R/03_augment.R
@@ -1,8 +1,8 @@
 ## Create merged (OTU counts and mapfile) dataframe
-merged_df <- read_tsv("data/otu_clean.tsv", show_col_types = FALSE) %>%
-  pivot_longer(matches("A\\d{2}"), names_to = "SampleID", values_to = "count") %>%
-  pivot_wider(id_cols = SampleID, names_from = family, values_from = count) %>%
+merged_df <- read_tsv("data/otu_clean.tsv", show_col_types = FALSE) %>%                   
+  pivot_longer(matches("A\\d{2}"), names_to = "SampleID", values_to = "count") %>%        # pivot columns that match A+2 digits to one column SampleID
+  pivot_wider(id_cols = SampleID, names_from = family, values_from = count) %>%           # pivot row names to column names (families)
   rename(Unknown="NA") %>%
-  full_join(x=read_tsv("data/map_clean.tsv", show_col_types = FALSE), by = "SampleID")
+  full_join(x=read_tsv("data/map_clean.tsv", show_col_types = FALSE), by = "SampleID")    # create join
 
-write_tsv(merged_df, "data/otu_map_merged.tsv")
+write_tsv(merged_df, "data/otu_map_merged.tsv")                                           # write file
