399527cdf7cb4f92cc4ce0e7dd6f0bbbf594f6a1
jakob949 <s213428@dtu.dk>
2022-05-07 13:54:48 +0000

    Try

--- R/08_model_v.R
+++ R/08_model_v.R
@@ -1,26 +1,57 @@
-NMDS_three <- read_tsv("data/otu.tsv")
-NMDS_three_meta <- read_tsv("data/map.tsv")
+# Reading originale data for NMDS analysis.
+NMDS_oto_data <- read_tsv("data/otu.tsv")
+NMDS_map <- read_tsv("data/map.tsv")
 
-otu_norm <- NMDS_three %>%
+# Normalizes data (relative abundances) and wrangles data
+NMDS_oto_normalized <- NMDS_oto_data %>%
   select(-"Consensus Lineage") %>%
-  mutate_at(vars(-`#OTU ID`), funs(./sum(.)*100)) %>% #normalize to relative abundance 
+  mutate_at(vars(-`#OTU ID`), funs(./sum(.)*100)) %>%  
+  # . = all data points. 
+  # => Mutate all data (except OTU ID) with funs() finding relative abundances
   gather(SampleID, abundance, `A01`:`A30`) %>%
   spread(key = `#OTU ID`,value = 'abundance') %>%
-  right_join(NMDS_three_meta %>% 
+  right_join(NMDS_map %>% 
                select(SampleID)) %>%
   column_to_rownames("SampleID")
 
-NMDS_ord <- otu_norm %>%
-  metaMDS(k=2)
+# Running the NMDS analysis from the vegan-library. With k=2 (two dimensions)
+NMDS_analysis <- NMDS_oto_normalized %>%
+  metaMDS(k=2, 
+          distance = "bray",
+          trymax = 100000,
+          try =200)
 
-NMDS_coords <- NMDS_ord[["points"]] %>%
+# Extracting the NMDS values for the two dimensions 
+NMDS_values <- NMDS_analysis[["points"]] %>%
   as_tibble(rownames = "SampleID") %>%
-  left_join(NMDS_three_meta)
+  left_join(NMDS_map)
 
+# Plotting the NMDS values:
+NMDS_plot <- ggplot(data = NMDS_values,
+       aes(x = MDS1, 
+           y = MDS2, 
+           shape = factor(Antibiotic), 
+           label = Name)) +
+  geom_point(size=1.5, alpha= 0.5) 
+  # geom_text_repel is a special geom, not included in the usual tidyverse packages
+  # geom_text_repel adds a nice feature to the normal geom_text. 
+  # Where it tries to find the position for he labels, so that the labels 
+  # overlap as little as possible. 
+  # geom_text_repel()
 
-NMDS_plot <- ggplot(data = NMDS_coords,
-       aes(x = MDS1, y = MDS2, shape = factor(Donor), label = Name)) +
-  geom_point(size=1.5, alpha= 0.5) + 
-  geom_text_repel()
+NMDS_plot
+
 
 ggsave("results/NMDS_plot.png")
+
+
+library("phyloseq"); packageVersion("phyloseq")
+library("plyr"); packageVersion("plyr")
+
+x<-ordinate(NMDS_oto_data, method = "NMDS", distance = "bray", trymax = 50)
+
+phyloseq_average(NMDS_oto_data)
+
+otu_mat <- NMDS_oto_data %>%
+  tibble::column_to_rownames("#OTU ID") 
+
