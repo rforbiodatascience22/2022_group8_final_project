e7a6464bca2aafc49d9c5582090ab255186d85d5
serestech <s212716@dt.dk>
2022-05-08 13:44:27 +0000

    tidying 01_load

--- R/01_load.R
+++ R/01_load.R
@@ -1,45 +1,38 @@
 # Checking and installing needed libraries:
 packages <- c("tidyverse", "vegan", "ggrepel", "BiocManager", "scales", "patchwork")
 install.packages(setdiff(packages, rownames(installed.packages()))) 
+#BiocManager::install("phyloseq")
 
-# Loading libraries 
+# Loading libraries
 library(tidyverse)
 library(vegan)
 library(ggrepel)
 library(BiocManager)
 library(scales)
 library(patchwork)
-# BiocManager::install("phyloseq")
+library(phyloseq)
+
 
 ## CFU counts file 
 
 # Read file (csv) as a tibble
 cfu_df <- read_csv("_raw/cfu_counts.csv", show_col_types = FALSE)
-# Write a tsv file
+# Write as tsv file in data folder
 write_tsv(cfu_df, file='data/cfu.tsv')
 
-## Mapfile
 
-# Read first line of mapfile to extract the column names as a character vector
-map_df_cols <- read_lines("_raw/mapfile.txt",n_max=1) %>%
-  str_split("\\t",simplify = TRUE)
-# Read the rest of the lines (skip both first and empty ones)
-map_df <- read_lines("_raw/mapfile.txt",skip=1,skip_empty_rows = TRUE) %>%
-  as_tibble_col(column_name = "lines") %>%
-  separate(lines,map_df_cols,sep="\\t")
-# Write a tsv file
+## Mapping file
+
+# Read tab-separated text file
+map_df <- read_tsv("_raw/mapfile.txt", show_col_types = FALSE)
+# Write a tsv file in data folder
 write_tsv(map_df, file='data/map.tsv')  
  
 
-## OTU file
+## OTU counts file
 
-# Read first line of otufile to extract the column names as a character vector
-otu_df_cols <- read_lines("_raw/otufile.txt",n_max=1) %>%
-  str_split("\\t",simplify = TRUE)
-# Read the rest of the lines (skip both first and empty ones)
-otu_df <- read_lines("_raw/otufile.txt",skip=1,skip_empty_rows = TRUE) %>%
-  as_tibble_col(column_name = "lines") %>%
-  separate(lines,otu_df_cols,sep="\\t")
-# Write a tsv file
+# Read tab-separated text file as tibble
+otu_df <- read_tsv("_raw/otufile.txt", show_col_types = FALSE)
+# Write as tsv file in data folder
 write_tsv(otu_df, file='data/otu.tsv')
 
