916e5f4e17a51380aec36fde3cb3922b3a8d6f0f
serestech <s212716@dt.dk>
2022-05-08 14:29:57 +0000

    simplify otu and map file

--- R/02_clean.R
+++ R/02_clean.R
@@ -1,32 +1,35 @@
 ## Clean CFU counts dataframe
 cfu_df_clean <- read_tsv("data/cfu.tsv", show_col_types = FALSE) %>%
-  separate(Treatment, c("Donor","Community","Antibiotic"), sep="[CA]") %>%              # separate "treatment" column into 3 by C and A
-  mutate(Donor = str_remove(Donor, "D")) %>%                                            # leave only numbers in "donor" column
-  select(-total_cfu,Dilution) %>%                                                       # select all columns but the ones specified
-  rename(cfu_ml = per_ml)                                                               # rename column
+  separate(Treatment, c("Donor","Community","Antibiotic"), sep="[CA]") %>%
+  mutate(Donor = str_remove(Donor, "D")) %>%
+  select(-total_cfu,Dilution) %>%
+  rename(cfu_ml = per_ml)
 # Write tsv file
 write_tsv(cfu_df_clean, file='data/cfu_clean.tsv')
 
 
 ## Clean mapfile dataframe
-map_df_clean <- read_tsv("data/map.tsv", show_col_types = FALSE) %>%                    # select all columns but the ones specified
-  select(-c(Experiment,Platform,CycleKit,RunDate,Project,Treat),                    
-         -contains("Sequence"),-contains("Index"),-contains("Description")) %>%
-  mutate(Donor=str_remove(Donor, "HumanDonor"))                                         # keep only donor number
+map_df_clean <- read_tsv("data/map.tsv", show_col_types = FALSE) %>%
+  mutate(Donor=str_remove(Donor, "HumanDonor"),
+         Treatment=case_when(Name=="Inoculum" ~ "Inoculum",
+                             Antibiotic=="none" ~ "None",
+                             TRUE ~ Antibiotic),
+         Name=case_when(Name=="Inoculum" ~ Name,
+                        TRUE ~ str_extract(Name, "\\d$"))) %>%
+  rename(Replicate=Name) %>%
+  select(c(SampleID,Donor,Replicate,Treatment))
 # Write tsv file
 write_tsv(map_df_clean, file='data/map_clean.tsv')
 
 
 ## Clean OTU counts dataframe
 otu_df_clean <- read_tsv("data/otu.tsv", show_col_types = FALSE) %>%
-  mutate(`Consensus Lineage` = str_remove_all(`Consensus Lineage`, "[a-z]_{2}")) %>%   # remove low bars between taxons
-  separate(col = `Consensus Lineage`, into = c('realm', 'phylum', 'class',             # and separate them by ";", giving new column names
+  mutate(`Consensus Lineage` = str_remove_all(`Consensus Lineage`, "[a-z]_{2}")) %>%
+  separate(col = `Consensus Lineage`, into = c('realm', 'phylum', 'class',
                                                'order', 'family', 'genus',
-                                               'species'), sep = ';' ) %>% 
-  select(-c(`#OTU ID`, genus, species, phylum, realm, class, order)) %>%               # select all columns but the ones specified
-  rowwise() %>%                                                                        # for each row
-  filter(sum(across(matches("A\\d\\d")))>=5) %>%                                       # only select rows that the sum of all samples is greater than 5
-  group_by(family) %>%
-  summarise_all(sum)                                                                   # show only family and sample columns
+                                               'species'), sep = ";") %>%
+  rename(OTU=`#OTU ID`) %>%
+  rowwise() %>%
+  filter(sum(across(matches("A\\d{2}")))>=5)
 # Write tsv file
 write_tsv(otu_df_clean, file='data/otu_clean.tsv')
